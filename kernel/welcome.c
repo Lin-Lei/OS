5d2a70f07212f1a166d34700b4195a55111c5fb3
Lin-Lei <873003680@qq.com>
2018-09-10 20:20:24 +0300

    改好界面

---
+++ kernel/welcome.c
@@ -0,0 +1,56 @@
+#include "type.h"
+#include "stdio.h"
+#include "const.h"
+#include "protect.h"
+#include "string.h"
+#include "fs.h"
+#include "proc.h"
+#include "tty.h"
+#include "console.h"
+#include "global.h"
+#include "proto.h"
+
+void welcomeMiao(){
+	int t=5000;
+	
+	
+	printf("  _        _____   _   _   _        ______   _____ \n");
+	milli_delay(t);
+	printf(" | |      |_   _| | \ | | | |      |  ____| |_   _|\n");
+	milli_delay(t);
+	printf(" | |        | |   |  \| | | |      | |__      | |  \n");
+	milli_delay(t);
+	printf(" | |        | |   | . ` | | |      |  __|     | |  \n");
+	milli_delay(t);
+	printf(" | |____   _| |_  | |\  | | |____  | |____   _| |_ \n");
+	milli_delay(t);
+	printf(" |______| |_____| |_| \_| |______| |______| |_____|\n");
+	milli_delay(t);
+	printf("\n");
+	milli_delay(t);
+	printf("\n");
+	milli_delay(t);
+	printf("\n");
+	milli_delay(t);
+	printf("\n");
+	milli_delay(t);
+	printf("\n");
+}
+
+PUBLIC void welcome(){
+	
+	printf("  _      _____ _   _ _      ______ _____ \n"); 
+	printf(" | |    |_   _| \ | | |    |  ____|_   _|\n"); 
+	printf(" | |      | | |  \| | |    | |__    | |  \n"); 
+	printf(" | |      | | | . ` | |    |  __|   | |  \n"); 
+	printf(" | |____ _| |_| |\  | |____| |____ _| |_ \n"); 
+	printf(" |______|_____|_| \_|______|______|_____|\n");  
+	printf(" ________________________________________\n");
+	printf("|                                        |\n");
+	printf("|                  MyOS                  |\n");
+	printf("|                                        |\n");
+	printf("|                Lin Lei                 |\n");
+	printf("|________________________________________|\n");
+	printf("\n");
+	printf("\n");
+}
